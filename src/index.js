56e1c0edbb3dedea84823c43ea5921f8b718fbc2
Jaron <jjkenyon777@gmail.com>
2021-12-05 14:27:12 -0700

    code setup

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// If you exawant to start measuring performance in your app, pass a function
+// to log results (for mple: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
